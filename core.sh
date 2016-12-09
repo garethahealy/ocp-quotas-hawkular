dd859b2bca99effb82cc7ec9a2ef8009e615d3ca
Gareth Healy <garethahealy@gmail.com>
2016-12-09 16:55:23 +0000

    Added code to create metrics

--- core.sh
+++ core.sh
@@ -13,12 +13,67 @@ for project in "${PROJECTS_ARRAY[@]}" ; do
 
         for ((i = 0; i < $QUOTA_COUNT; i++)); do
             QUOTA=$(echo $QUOTA_ARRAY | jq --arg i "$i" '.items[$i | tonumber]')
-            QUOTA_HARD=$(echo $QUOTA | jq '.status.hard')
-            QUOTA_USED=$(echo $QUOTA | jq '.status.used')
+            QUOTA_NAME=$(echo $QUOTA | jq '.metadata.name' | tr -d '"')
+            QUOTA_NAMESPACE=$(echo $QUOTA | jq '.metadata.namespace' | tr -d '"')
 
-            echo $QUOTA_HARD
-            echo $QUOTA_USED
+            QUOTA_HARD=$(echo $QUOTA | jq '.status.hard | to_entries')
+            if [ ! -z ${QUOTA_HARD+x} ]; then
+                QUOTA_HARD_COUNT=$(echo $QUOTA_HARD | jq length)
+                for ((j = 0; j < $QUOTA_HARD_COUNT; j++)); do
+                    ITEM=$(echo $QUOTA_HARD | jq --arg j "$j" '.[$j | tonumber]')
+                    ITEM_KEY=$(echo $ITEM | jq '.key' | tr -d '"')
+                    ITEM_VALUE=$(echo $ITEM | jq '.value' | tr -d '"')
+
+                    METRIC_ID=$QUOTA_NAMESPACE/$QUOTA_NAME/$ITEM_KEY
+                    echo "Hard; $METRIC_ID/$ITEM_VALUE"
+
+                    CREATE_JSON_PAYLOAD=$(cat <<EOF
+{
+  "id" : "$METRIC_ID",
+  "tags" : {
+    "namespace" : "$QUOTA_NAMESPACE",
+    "quota" : "true"
+  },
+  "type" : "counter",
+  "tenantId" : "custom"
+}
+EOF
+)
+
+                    ADD_JSON_PAYLOAD=$(cat <<EOF
+[{
+  "id" : "$METRIC_ID",
+  "tags" : {
+    "namespace" : "$QUOTA_NAMESPACE",
+    "quota" : "true"
+  },
+  "type" : "counter",
+  "tenantId" : "custom",
+  "dataPoints" : [ {
+    "timestamp" : $(date +%s),
+    "value" : $ITEM_VALUE,
+    "tags" : {
+      "namespace" : "$QUOTA_NAMESPACE",
+      "quota" : "true"
+    }
+  } ]
+}]
+EOF
+)
+
+                    # Create metric and the post datapoint
+                    echo $CREATE_JSON_PAYLOAD | curl -k -s -f -X POST -H "Content-Type: application/json" -H "Hawkular-Tenant: custom" -H "Authorization: Bearer EKEOuAiS9hRGXQgECzdnRK0J3WRDy8gKItkcCsLE81M" https://hawkular-metrics.apps.10.2.2.2.xip.io/hawkular/metrics/counters -d @-
+                    echo $ADD_JSON_PAYLOAD | curl -k -X POST -H "Content-Type: application/json" -H "Hawkular-Tenant: custom" -H "Authorization: Bearer EKEOuAiS9hRGXQgECzdnRK0J3WRDy8gKItkcCsLE81M" https://hawkular-metrics.apps.10.2.2.2.xip.io/hawkular/metrics/counters/raw -d @-
+                done
+            fi
+
+            QUOTA_USED=$(echo $QUOTA | jq '.status.used | to_entries')
+            if [ ! -z ${QUOTA_USED+x} ]; then
+                echo "Used RAW"
+                #$QUOTA_USED
+            fi
         done
     fi
 done
+
 
